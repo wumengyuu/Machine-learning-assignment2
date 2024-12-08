b62d57b6c9755217dcda48d11eea6aa2f2e033f9
FPato <patrik.filipiak1@gmail.com>
2024-12-08 21:29:49 +0000

    adding plot error function

--- plot_y_yhat.py
+++ plot_y_yhat.py
@@ -17,3 +17,37 @@ def plot_y_yhat(y_val,y_pred, plot_title = "plot"):
     plt.plot([x0, x1], [x0, x1], color='red')
     plt.axis('square')
     plt.show()
+
+    def plot_error(error: dict, plot_title = "Plot", xlabel = "X", ylabel = "Y"):
+    plt.plot(list(error.keys()), list(error.values()))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(plot_title)
+    plt.show()
+
+
+def plot_all_error_stats(data: dict, plot_title = "Plot", xlabel = "X", ylabel = "Y"):
+    keys = list(data.keys())
+    means = [np.mean(values) for values in data.values()]
+    std_devs = [np.std(values) for values in data.values()]
+    mins = [np.min(values) for values in data.values()]
+    maxs = [np.max(values) for values in data.values()]
+    x = range(len(keys))
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, means, label='Mean', color='blue', marker='o', linewidth=2)
+    plt.plot(x, mins, label='Min', color='red', marker='o', linewidth=2)
+    plt.plot(x, maxs, label='Max', color='red', marker='o', linewidth=2)
+
+    plt.fill_between(x,
+                     [m - s for m, s in zip(means, std_devs)],
+                     [m + s for m, s in zip(means, std_devs)],
+                     color='blue', alpha=0.2, label='± Std Dev')
+
+    plt.xticks(x, keys)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(plot_title)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
